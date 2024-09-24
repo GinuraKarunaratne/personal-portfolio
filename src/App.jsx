6f6529d1dee12595ad11d107fcbea294b7f29fee
Ginura Karunaratne <iamginurakarunaratne@gmail.com>
2024-09-24 13:17:23 +0530

    Added some details and minor design changes and added a custom pointer.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react';
+import './App.css';
+
+function App() {
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+  const [isVisible, setIsVisible] = useState(true);
+
+  useEffect(() => {
+    let timeoutId;
+
+    const handleMouseMove = (e) => {
+      setPosition({ x: e.clientX, y: e.clientY });
+      setIsVisible(true);
+
+      clearTimeout(timeoutId);
+      timeoutId = setTimeout(() => {
+        setIsVisible(false);
+      }, 2000);
+    };
+
+    window.addEventListener('mousemove', handleMouseMove);
+
+    return () => {
+      window.removeEventListener('mousemove', handleMouseMove);
+      clearTimeout(timeoutId);
+    };
+  }, []);
+
+  return (
+    <>
+      <div className='p1'>
+        <h1>Ginura Karunaratne</h1>
+      </div>
+
+      <div
+        className="custom-cursor"
+        style={{
+          top: `${position.y}px`,
+          left: `${position.x}px`,
+          opacity: isVisible ? 1 : 0,
+        }}
+      ></div>
+    </>
+  );
+}
+
+export default App;
